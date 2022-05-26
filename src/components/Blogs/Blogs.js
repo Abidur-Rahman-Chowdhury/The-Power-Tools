0643fd0fa01a9ab843807571f6ad2ad980bcd712
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-26 18:50:08 +0600

    update readme file

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -9,9 +9,9 @@ const Blogs = () => {
         </h2>
         <div className="grid grid-cols-1 gap-5 place-items-center md:grid-cols-2 lg:grid-cols-3">
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   1. How will you improve the performance of a React
                   Application?
                 </h2>
@@ -28,9 +28,9 @@ const Blogs = () => {
             </div>
           </div>
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   2. What are the different ways to manage a state in a React
                   application?
                 </h2>
@@ -45,9 +45,9 @@ const Blogs = () => {
             </div>
           </div>
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   3. How does prototypical inheritance work?
                 </h2>
 
@@ -64,9 +64,9 @@ const Blogs = () => {
             </div>
           </div>
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   4. Why you do not set the state directly in React. For
                   example, if you have const [products, setProducts] =
                   useState([]). Why you do not set products = [...] instead, you
@@ -86,9 +86,9 @@ const Blogs = () => {
             </div>
           </div>
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   5. You have an array of products. Each product has a name,
                   price, description, etc. How will you implement a search to
                   find products by name?
@@ -110,9 +110,9 @@ const Blogs = () => {
             </div>
           </div>
           <div>
-            <div class="card max-w-lg bg-base-100 shadow-xl">
-              <div class="card-body">
-                <h2 class="card-title">
+            <div className="card max-w-lg bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
                   6. What is a unit test? Why should write unit tests?
                 </h2>
 
