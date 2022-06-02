7cef4e36dd7d3deaf7f8f36851480330323c7c18
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-06-02 12:43:41 +0600

    fixed about  section

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -10,20 +10,13 @@ const About = () => {
       </div>
 
       <p className="text-center text-xl mt-4 px-16">
-        THE POWER TOOLS IS A TOOLS BRAND ABOUT PROVIDING TOOLS FOR EVERY HOME. DISTRIBUTING
-        HIGH END TOOLS TO HELP CREATE YOUR IDEAL VISION FOR A PROJECT, HOME, OR
-        APPLICATION. WE ARE YOUR PARTNERS, CONFIDANTS AND MOST IMPORTANTLY YOUR
-        GUIDANCE TO HELP YOU COMPLETE PROJECTS THAT MATTER. WE ARE TENACIOUS,
-        EXPRESSIVE, DEDICATED HIGHLY SPECIALIZED IN THE TOOLS SPACE ACROSS
-        VARIOUS APPLICATIONS. WE WANT TO SUSTAINABLY DEVELOP WITH OUR
-        INTERNATIONAL PARTNERS BY OFFERING THEM TOOLS THAT WIN EVERY SINGLE
-        TIME. IT’S THE LITTLE THINGS THAT MAKE A DIFFERENCE, DETAILS MATTER, YOU
-        MATTER, AND TOGETHER WE’RE BRINGING EXPERTISE TO OUR CLIENTS THROUGH
-        PARTNERSHIP FOR LIFE. WE WANT TO PARTNER WITH YOU, WE WANT YOU TO CHOOSE
-        US AS WE CHOOSE YOU. WE KNOW THINGS AREN’T ALWAYS CLEAR, THINGS GET
-        MESSY, BUT OUR TEAM WITH OVER YEARS OF EXPERIENCE DEALING WITH CLIENTS
-        ACROSS THE WORLD INTERNATIONALLY AND CONSTANTLY FINDING WAYS TO OFFER
-        FLEXIBLE SOLUTIONS THAT WORK
+        The Power Tools founded in Bangladesh in 2022, and as a professional
+        manufacturer we specializes in producing various hand tools (sockets,
+        tool sets, etc.), pneumatic accessories, and automotive repairing tools,
+        including customized tools. Now we serve our clients with over 2,000
+        items to meet any request from their markets. Besides regular hand
+        tools, we focus on developing unique auto-repair tools for the
+        international brand vehicle companies in Europe and America.
       </p>
     </div>
   );
