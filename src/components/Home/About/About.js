edc5ef32f35dbef2ade1965af7ffa64ddbd86127
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-07-22 12:15:34 +0600

    fixed bug on about section

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,22 +1,31 @@
 import React from 'react';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 const About = () => {
   return (
-    <div className="mb-5">
-      <div className="text-center">
+    <div className="mb-5" >
+      <div className="text-center" >
         <h2 className="text-3xl italic text-center font-bold border-b-2 inline-block ">
           About Us
         </h2>
       </div>
 
       <p className="text-center text-xl mt-4 px-16">
-        The Power Tools founded in Bangladesh in 2022, and as a professional
-        manufacturer we specializes in producing various hand tools (sockets,
-        tool sets, etc.), pneumatic accessories, and automotive repairing tools,
-        including customized tools. Now we serve our clients with over 2,000
-        items to meet any request from their markets. Besides regular hand
-        tools, we focus on developing unique auto-repair tools for the
-        international brand vehicle companies in Europe and America.
+        THE POWER TOOLS IS A TOOLS BRAND ABOUT PROVIDING TOOLS FOR EVERY HOME. DISTRIBUTING
+        HIGH END TOOLS TO HELP CREATE YOUR IDEAL VISION FOR A PROJECT, HOME, OR
+        APPLICATION. WE ARE YOUR PARTNERS, CONFIDANTS AND MOST IMPORTANTLY YOUR
+        GUIDANCE TO HELP YOU COMPLETE PROJECTS THAT MATTER. WE ARE TENACIOUS,
+        EXPRESSIVE, DEDICATED HIGHLY SPECIALIZED IN THE TOOLS SPACE ACROSS
+        VARIOUS APPLICATIONS. WE WANT TO SUSTAINABLY DEVELOP WITH OUR
+        INTERNATIONAL PARTNERS BY OFFERING THEM TOOLS THAT WIN EVERY SINGLE
+        TIME. IT’S THE LITTLE THINGS THAT MAKE A DIFFERENCE, DETAILS MATTER, YOU
+        MATTER, AND TOGETHER WE’RE BRINGING EXPERTISE TO OUR CLIENTS THROUGH
+        PARTNERSHIP FOR LIFE. WE WANT TO PARTNER WITH YOU, WE WANT YOU TO CHOOSE
+        US AS WE CHOOSE YOU. WE KNOW THINGS AREN’T ALWAYS CLEAR, THINGS GET
+        MESSY, BUT OUR TEAM WITH OVER YEARS OF EXPERIENCE DEALING WITH CLIENTS
+        ACROSS THE WORLD INTERNATIONALLY AND CONSTANTLY FINDING WAYS TO OFFER
+        FLEXIBLE SOLUTIONS THAT WORK
       </p>
     </div>
   );
