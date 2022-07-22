0f8ef1c39c9f778d9ea45bbac9f9f2adc545f3b1
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-07-22 12:27:22 +0600

    fixed about section

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -12,20 +12,8 @@ const About = () => {
       </div>
 
       <p className="text-center text-xl mt-4 px-16">
-        THE POWER TOOLS IS A TOOLS BRAND ABOUT PROVIDING TOOLS FOR EVERY HOME. DISTRIBUTING
-        HIGH END TOOLS TO HELP CREATE YOUR IDEAL VISION FOR A PROJECT, HOME, OR
-        APPLICATION. WE ARE YOUR PARTNERS, CONFIDANTS AND MOST IMPORTANTLY YOUR
-        GUIDANCE TO HELP YOU COMPLETE PROJECTS THAT MATTER. WE ARE TENACIOUS,
-        EXPRESSIVE, DEDICATED HIGHLY SPECIALIZED IN THE TOOLS SPACE ACROSS
-        VARIOUS APPLICATIONS. WE WANT TO SUSTAINABLY DEVELOP WITH OUR
-        INTERNATIONAL PARTNERS BY OFFERING THEM TOOLS THAT WIN EVERY SINGLE
-        TIME. IT’S THE LITTLE THINGS THAT MAKE A DIFFERENCE, DETAILS MATTER, YOU
-        MATTER, AND TOGETHER WE’RE BRINGING EXPERTISE TO OUR CLIENTS THROUGH
-        PARTNERSHIP FOR LIFE. WE WANT TO PARTNER WITH YOU, WE WANT YOU TO CHOOSE
-        US AS WE CHOOSE YOU. WE KNOW THINGS AREN’T ALWAYS CLEAR, THINGS GET
-        MESSY, BUT OUR TEAM WITH OVER YEARS OF EXPERIENCE DEALING WITH CLIENTS
-        ACROSS THE WORLD INTERNATIONALLY AND CONSTANTLY FINDING WAYS TO OFFER
-        FLEXIBLE SOLUTIONS THAT WORK
+        This is our tools manufacture website where we provide best tools for our daily need. We keep in my mind that we are a family business and we are always ready to help our customers. We like to provide our customers with the best quality tools at the lowest price. We are always ready to help our customers with any queries. We never compromise on the quality of our tools. Customer satisfaction is our top priority.
+        
       </p>
     </div>
   );
