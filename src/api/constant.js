0ad93550a8e03afb40a28a3a49db7e79c05e65b6
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2023-01-05 23:40:49 +0600

    shift to render change base url

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,2 +1,3 @@
-export const baseUrl = `https://pacific-sierra-58936.herokuapp.com`
+// export const baseUrl = `https://pacific-sierra-58936.herokuapp.com`
+export const baseUrl = `https://the-power-tools-server.onrender.com`
 // export const baseUrl = `http://localhost:5000`;
